c8c1697b0de05466dd461c39e03dd51b161b423d
Zhuofei Kang <kang.zhuofei@gmail.com>
2018-12-05 10:08:29 -0500

    Update regression.py

--- models/regression.py
+++ models/regression.py
@@ -6,11 +6,19 @@ from torch.nn import init
 import pdb
 
 class RegressionModel(nn.Module):
-	def __init__(self, c_in=1, nlabels=10): # you might need to change function argument
-		super(CNNModel, self).__init__()
+	def __init__(self, c_in=3, input_size=32, nlabels=10): # you might need to change function argument
+		super(RegressionModel, self).__init__()
 		# add code here
-		pass
+		self.nlabels = nlabels
+		self.input_size = input_size
+		self.linear = nn.Linear(input_size*input_size*c_in, nlabels)
+		#pass
 
-	def forward(self): # you might need to change function argument
+	def forward(self, x): # you might need to change function argument
 		# add code here
-		pass
+		#pass
+		batch_size = x.size(0)
+		x = x.view(batch_size, -1)
+		out = self.linear(x)
+		return out
+#python main.py -lr 0.01 --epochs 10 --weight_decay 5e-4 --model_name Reg --model Regression --optimizer SGD --dataset CIFAR
